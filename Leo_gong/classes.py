b344a83bc4b2e3bc48d3cf97d707f90dbcde77cb
Adam Saher <adamsaher@Adams-MacBook-Pro.local>
2022-02-24 20:28:30 -0500

    leo

--- Leo_gong/classes.py
+++ Leo_gong/classes.py
@@ -16,6 +16,9 @@ price, and current price.
 ■ A method named getChangePercent() that returns the percentage changed
 from previousClosingPrice to currentPrice.
 """
+import math
+
+
 class Stock:
     def __init__(self, symbol, name, previousClosingPrice, currentPrice):
         self.symbol = symbol
@@ -79,10 +82,144 @@ class Account:
 default is False).
 ■ A private float data field named radius that specifies the radius of the fan.
 ■ A private string data field named color that specifies the color of the fan.
-■ The accessor and mutator methods for all four data fields.
+
 ■ A constructor that creates a fan with the specified speed (default SLOW), radius
 (default 5), color (default blue), and on (default False).
 Write a test program that creates two Fan objects. For the first object, assign the maximum speed, radius 10, color 
 yellow, and turn it on. Assign medium speed, radius 5, color blue, and turn it off for the second object. Display each 
 object’s speed, radius, color, and on properties.
 """
+class Fan:
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+    def __init__(self, speed, power, radius, color):
+        self.speed = speed
+        self.power = power
+        self.radius = radius
+        self.color = color
+
+"""
+(Geometry: n-sided regular polygon) An n-sided regular polygon’s sides all have the same length and all of its angles 
+have the same degree (i.e., the polygon is both equilateral and equiangular). Design a class named RegularPolygon 
+that contains:
+■ A private int data field named n that defines the number of sides in the polygon.
+■ A private float data field named side that stores the length of the side.
+■ A private float data field named x that defines the x-coordinate of the center of
+the polygon with default value 0.
+■ A private float data field named y that defines the y-coordinate of the center of the polygon with default value 0.
+■ A constructor that creates a regular polygon with the specified n (default 3), side (default 1), x (default 0), and 
+y (default 0).
+■ The method getPerimeter() that returns the perimeter of the polygon.
+■ The method getArea() that returns the area of the polygon. The formula for
+computing the area of a regular polygon is Area = (n x s^2) / 4 x tan(π/n) 
+
+Write a test program that creates three RegularPolygon objects, created using RegularPolygon(), 
+using RegularPolygon(6, 4) and RegularPolygon(10, 4, 5.6, 7.8). For each object, display its perimeter and area.
+"""
+class RegularPolygon:
+
+    def __init__(self, n, side, x=0, y=0):
+        self.n = n
+        self.side = side
+        self.x = x
+        self.y = y
+
+    def getPerimeter(self):
+        perimeter = self.n * self.side
+        return round(perimeter, 2)
+
+    def getArea(self):
+        area = (self.n * self.side ** 2) / 4 * math.tan(math.pi / self.n)
+        return round(area, 2)
+
+
+"""
+(Algebra: quadratic equations) Design a class named QuadraticEquation for a quadratic equation ax2 + bx + x = 0. The 
+class contains:
+■ The private data fields a, b, and c that represent three coefficients.
+■ A constructor for the arguments for a, b, and c.
+■ A method named getDiscriminant() that returns the discriminant, which is b^2 - 4ac.
+■ The methods named getRoot1() and getRoot2() for returning the two roots
+of the equation using these formulas:
+
+r1 =(-b+ sqrt(b^2-4ac)) / 2a and r2 = (-b-sqrt(b^2-4ac)) / 2a
+
+These methods are useful only if the discriminant is non-negative. Let these methods return 0 if the discriminant is 
+negative.
+Implement the class. Write a test program that prompts the user to enter values for a, b, and c and displays the result 
+based on the discriminant. If the discriminant is positive, display the two roots. If the discriminant is 0, display the 
+one root. Otherwise, display “The equation has no roots.” 
+
+Sample Run 1:
+Enter a, b, c: 1.0, 3, 1
+The roots are -0.381966 and -2.61803
+
+Sample Run 2:
+Enter a, b, c: 1, 2.0, 1
+The root is -1
+
+Sample Run 3:
+Enter a, b, c: 1, 2, 3
+The equation has no real roots
+"""
+class QuadraticEquation:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def getDiscriminant(self):
+        return self.b ** 2 - 4 * self.a * self.c
+
+    def getRoot1(self):
+        return round(((-self.b + math.sqrt(self.b ** 2 - 4 * self.a * self.c)) / 2 * self.a), 6)
+
+    def getRoot2(self):
+        return round(((-self.b - math.sqrt(self.b ** 2 - 4 * self.a * self.c)) / 2 * self.a), 6)
+
+
+"""
+(Algebra: 2 * 2 linear equations) Design a class named LinearEquation for a 2 * 2 system of linear equations:
+
+ax + by = e
+cx + dy = f
+
+x = (ed - bf) / (ad - bc)
+y = (af - ec) / (ad - bc)
+
+
+The class contains:
+■ The private data fields a, b, c, d, e, and f with get methods.
+■ A constructor with the arguments for a, b, c, d, e, and f.
+■ A method named isSolvable() that returns true if ad - bc is not 0.
+■ The methods getX() and getY() that return the solution for the equation.
+Implement the class. Write a test program that prompts the user to enter a, b, c, d, e, and f and displays the result. 
+If ad - bc is 0, report that “The equation has no solution.”
+
+Sample Run 1:
+Enter a, b, c, d, e, f: 9.0, 4.0, 3.0, -5.0, -6.0, -21.0
+x is -2.0 and y is 3.0
+
+Sample Run 2:
+Enter a, b, c, d, e, f: 1.0, 2.0, 2.0, 4.0, 4.0, 5.0
+The equation has no solution
+"""
+class LinearEquation:
+    def __init__(self, a, b, c, d, e, f):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.e = e
+        self.f = f
+
+    def isSolvable(self):
+        if (self.a * self.d - self.b * self.c) == 0:
+            return False
+        else:
+            return True
+
+    "x = (ed - bf) / (ad - bc) y = (af - ec) / (ad - bc)"
+    def getX(self):
+        x = (self.e * self)
--- Leo_gong/main.py
+++ Leo_gong/main.py
@@ -1,36 +1,25 @@
-from classes import Account
-"""
-Write a program that reads an integer and displays all its smallest factors, also known as prime factors. For example, 
-if the input integer is 120, the output should be as follows:
-2, 2, 2, 3, 5
-"""
-# def is_prime(num):
-#    for x in range(2, num):
-#       if num % x == 0:
-#          return False
-#    return True
-#
-# def primes(num):
-#    lst = []
-#    for x in range(2, num):
-#       if is_prime(x):
-#          lst.append(x)
-#    return lst
-#
-# def factors(num):
-#    all_primes = primes(num)
-#    factors_lst = []
-#    for x in all_primes:
-#       while num % x == 0:
-#          factors_lst.append(x)
-#          num //= x
-#    return factors_lst
+from classes import QuadraticEquation
+
+
+
 
 if __name__ == '__main__':
-#print the id, balance, monthly interest rate, and monthly interest.
-    a = Account(1122, 20000, (4.5 / 100))
-    a.withdraw(2500)
-    a.deposit(3000)
-    m = a.getMonthlyInterestRate()
-    m2 = a.getMonthlyInterest()
-    print(a.id, a.balance, m, m2)
+
+    a, b, c = input("Enter a, b, c:").split(", ")
+    a = float(a)
+    b = float(b)
+    c = float(c)
+
+    q1 = QuadraticEquation(a, b, c)
+    discriminant = q1.getDiscriminant()
+
+    if discriminant < 0:
+        print("The equation has no roots.")
+    elif discriminant > 0:
+        r1 = q1.getRoot1()
+        r2 = q1.getRoot2()
+        print("The roots are", r1, "and", r2)
+    else:
+        print("The root is", q1.getRoot1())
+
+
