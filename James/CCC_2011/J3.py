062977a2dc34d32b113d97318b8329c661a3e54a
AZS-99 <adam.zachary.saher@gmail.com>
2022-05-20 18:29:26 -0400

    James

---
+++ James/CCC_2011/J3.py
@@ -0,0 +1,33 @@
+"""
+Problem Description
+In a sumac sequence, t1, t2, .., tm, each term is an integer greater than or equal 0. Also, each term, starting with the
+third, is the difference of the preceding two terms (that is, tn+2 = tn − tn+1 for n ≥ 1). The sequence terminates at
+tm if tm−1 < tm.
+For example, if we have 120 and 71, then the sumac sequence generated is as follows:
+                                        120, 71, 49, 22, 27.
+This is a sumac sequence of length 5.
+
+Input Specification
+The input will be two positive numbers t1 and t2, with 0 < t2 < t1 < 10000.
+
+Output Specification
+The output will be the length of the sumac sequence given by the starting numbers t1 and t2.
+
+Sample Input
+120
+71
+Output for Sample Input
+5
+"""
+def sumac():
+    file = open("CCC_2011/J3", "r")
+    s1 = int(file.readline())
+    s2 = int(file.readline())
+    os = s1
+    length = 2
+    while s1 - s2 < s1:
+        s1 = s1 - s2
+        s2 = os
+        os = s1
+        length += 1
+    print(length)
